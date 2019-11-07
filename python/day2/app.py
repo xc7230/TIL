47f3e4fa27978c4ebcca47a6de67b2bb6cc548ca
xc7230 <33945185+xc7230@users.noreply.github.com>
2019-11-07 16:02:16 +0900

    day2

--- python/day2/app.py
+++ python/day2/app.py
@@ -67,6 +67,27 @@ def receive():
     return render_template('receive.html', name=name, msg = message)
 
 
+#인디언
+@app.route('/indian')
+def indian_send():
+    return render_template('indian_send.html')
+
+
+@app.route('/indian_receive')
+def indian_receive():
+    date = request.args.get('date')
+    year = date[3:4]
+    month = date[5:7]
+    day = date[8:10]
+
+
+    year_arr = ['시끄러운, 말 많은', '푸른', '어두운', '조용한', '웅크린', '백색', '지혜로운', '용감한', '날카로운', '욕심 많은']
+    month_arr = ['늑대','태양','양','매','황소','불꽃','나무','달빛','말','돼지','하늘','바람']
+    day_arr  = ["의 환생","의 죽음","아래에서","를(을) 보라","이(가) 노래하다","그림자","의 일격","에게 쫒기는 남자","의 행진 ","의 왕","의 유령","을 죽인자","는(은) 맨날 잠잔다","처럼","의 고향","의 전사","은(는) 나의친구","의 노래","의 정령","의 파수꾼","의 악마","와(과) 같은 사나이","를(을) 쓰러트린자","의 혼 ","은(는) 말이 없다"]
+
+    name= year_arr[int(year)-1] + month_arr[int(month)-1] + day_arr[int(day)-1]
+
+    return render_template('indian_receive.html', name=name, date = date)
 
 
 if __name__=="__main__":
