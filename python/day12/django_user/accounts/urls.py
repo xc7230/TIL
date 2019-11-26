5c56432259914877b73e12e0f34f06a7e209306a
김현민 <xc7230@naver.com>
2019-11-26 14:03:47 +0900

    day12

--- python/day12/django_user/accounts/urls.py
+++ python/day12/django_user/accounts/urls.py
@@ -5,5 +5,7 @@ app_name="accounts"
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     
 ]
--- python/day12/django_user/accounts/views.py
+++ python/day12/django_user/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from IPython import embed
 
 def signup(request):
@@ -7,7 +9,8 @@ def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('boards:index')
     else:
         form = UserCreationForm()
@@ -17,3 +20,27 @@ def signup(request):
     }
 
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        #embed()
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('boards:index')
+
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+    return redirect('boards:index')
+
+    
